41baeb282f087280ccc6458a8a20ec68bb398444
Yulia Tsernant <yulia.tsernant@d-d.me>
2020-10-23 18:00:13 -0700

    New svg version

--- main.js
+++ main.js
@@ -1,70 +1,17 @@
-function loadParticles(color, opacity) {
-  particlesJS("particles-js", {
-    "particles": {
-      "number": {
-        "value": 355,
-        "density": {
-          "enable": true,
-          "value_area": 789
-        }
-      },
-      "color": {
-        "value": `${color}`,
-      },
-      "shape": {
-        "type": "circle",
-      },
-      "opacity": {
-        "value": opacity,
-        "random": false,
-        "anim": {
-          "enable": true,
-          "speed": 1,
-          "opacity_min": 0,
-          "sync": false
-        }
-      },
-      "size": {
-        "value": 2.5,
-        "random": true,
-        "anim": {
-          "enable": true,
-          "speed": 2,
-          "size_min": 0,
-          "sync": false
-        }
-      },
-      "line_linked": {
-        "enable": false,
-      },
-      "move": {
-        "enable": true,
-        "speed": 0.2,
-        "direction": "none",
-        "random": true,
-        "straight": false,
-        "out_mode": "out",
-        "bounce": false,
-        "attract": {
-          "enable": false,
-        }
-      }
-    },
-    "interactivity": {
-      "detect_on": "canvas",
-      "events": {
-        "onhover": {
-          "enable": false,
-          "mode": "bubble"
-        },
-        "onclick": {
-          "enable": false,
-        },
-        "resize": true
-      },
-    },
-    "retina_detect": true
-  });
-};
+function draw() {
+  let paths = document.querySelectorAll('#wardruna path');
 
-loadParticles('#EFEFEF', 0.8);
+  for (let i = 0; i < paths.length; i += 1) {
+    setStyle(paths[i], i);
+  };
+}
+
+function setStyle(element, i) {
+  setTimeout(() => {
+    element.setAttribute('style', `stroke: #c5c5c5; stroke-width: 20px; stroke-dasharray: ${element.getTotalLength()}px; stroke-dashoffset: ${element.getTotalLength()}px; animation: line-animation 1s linear forwards 1s`);
+  }, 500 * i)
+}
+
+draw()
+
+
